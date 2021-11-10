dd3e60d73640894afb565c8543fa38236267559e
Manuel Entrena <manuel.entrena@gmail.com>
2021-11-10 20:27:58 +0100

    #8 Create Controller for Article
    Repaso crud general de laravel en controlador con articulo

--- database/factories/ArticleFactory.php
+++ database/factories/ArticleFactory.php
@@ -2,7 +2,7 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
-use App\Article;
+use App\Http\Models\Article;
 use Faker\Generator as Faker;
 
 $factory->define(Article::class, function (Faker $faker) {
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use V1\ArticleController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+/* Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+}); */
+
+Route::prefix('v1')->group(function () {
+    Route::apiResource('articles', ArticleController::class, ['as' => 'v1']);
 });
