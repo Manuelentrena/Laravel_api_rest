cb0fc13b28dff5915f700cb17380c610a1e286c2
Manuel Entrena <manuel.entrena@gmail.com>
2021-11-18 19:55:07 +0100

    Added code

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -5,11 +5,7 @@ use Illuminate\Database\Seeder;
 
 class UserSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
+    
     public function run()
     {
       //Borra los datos
