d5b1c392d30a54503f8db69ad390547163075fe8
Manuel Entrena <manuel.entrena@gmail.com>
2021-11-18 20:24:52 +0100

    New change

--- routes/api.php
+++ routes/api.php
@@ -5,17 +5,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\V1\ArticleController;
 use App\Http\Controllers\V1\UserController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
 Route::group(['prefix' => 'v1','middleware' => ['jwt.verify']], function() {
     Route::apiResource('articles', ArticleController::class, ['as' => 'v1'])->except(['index']);
     Route::get('user', [UserController::class, 'getAuthenticatedUser'])->name('v1.getAuthenticatedUser');
