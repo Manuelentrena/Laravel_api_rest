0b1b9236cea99103707463923ede54a1ac6ca200
Manuel Entrena <manuel.entrena@gmail.com>
2021-12-28 19:33:00 +0100

    #45 Polymorphic Relationships part2

--- app/Http/Resources/User.php
+++ app/Http/Resources/User.php
@@ -8,21 +8,30 @@ use Illuminate\Support\Facades\Auth;
 
 class User extends JsonResource
 {
+
+  protected $token;
+
+  public function __construct($resource, $token = null){
+    parent::__construct($resource);
+    $this->token = $token;
+  }
+
   public function toArray($request)
   {
     return [
       'id' => $this->id,
       'name' => $this->name,
       'email' => $this->email,
-      // 'userable' => $this->userable,
-      // $this->merge($this->userable),
-      'type' => $this->when(Auth::user()->userable_type == 'App\Http\Models\Admin','Admin', 'Writer'),
-      'credential_number' => $this->when(Auth::user()->userable_type == 'App\Http\Models\Admin',$this->userable->credential_number),
-      'editorial' => $this->when(Auth::user()->userable_type == 'App\Http\Models\Writer',$this->userable->editorial),
-      'short_bio' => $this->when(Auth::user()->userable_type == 'App\Http\Models\Writer',$this->userable->short_bio),
+      //'userable' => $this->userable,
+      $this->merge($this->userable),
+      //'type' => $this->when(Auth::user()->userable_type == 'App\Http\Models\Admin','Admin', 'Writer'),
+      //'credential_number' => $this->when(Auth::user()->userable_type == 'App\Http\Models\Admin',$this->userable->credential_number),
+      //'editorial' => $this->when(Auth::user()->userable_type == 'App\Http\Models\Writer',$this->userable->editorial),
+      //'short_bio' => $this->when(Auth::user()->userable_type == 'App\Http\Models\Writer',$this->userable->short_bio),
       'articles' => Article::where('user_id', '=', $this->id)->get(),
       'created_at' => $this->created_at,
       'updated_at' => $this->updated_at,
+      'token' => $this->when($this->token,$this->token),
     ];
   }
 }
--- database/seeds/UserAdminSeeder.php
+++ database/seeds/UserAdminSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Models\User;
+use App\Http\Models\Admin;
 use Illuminate\Database\Seeder;
 
 class UserAdminSeeder extends Seeder
@@ -9,13 +10,16 @@ class UserAdminSeeder extends Seeder
     {
       //Borra el admin anterior
       User::where('name', '=', 'Administrador')->delete();
+      // Creamos data en Admin Rol
+      $admin = Admin::create(['credential_number' => '123123123123']);
       // Creamos un user a parte como Admin
-      User::create([
+      $admin->user()->create([
         'name' => 'Administrador',
         'email' => 'admin@gmail.com',
         'password' => Hash::make('123456'),
         'email_verified_at' => now(),
         'remember_token' => Str::random(10),
+        'role' => 'ROLE_ADMIN',
       ]);
     }
 }
